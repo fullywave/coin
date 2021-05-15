5aab407c482ddfe7dc8ec6b82be40d22892dd08c
xujianlin <xujianlin@miduchina.com>
2021-05-16 03:23:50 +0800

    modify

--- 1.29/doge.sh
+++ 1.29/doge.sh
@@ -1,2 +1,3 @@
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:13333 --user DOGE:DSjjZZnVi9iSxTXFHXXY1S5CccgrENLQpK.fullwave#3cuh-7lhp --4g-alloc-size 4024 --keepfree 8 
+cd "$(dirname "$0")"
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:13333 --user DOGE:DSjjZZnVi9iSxTXFHXXY1S5CccgrENLQpK.fullwave#3cuh-7lhp --4g-alloc-size 4024 --keepfree 8
 timeout 10
